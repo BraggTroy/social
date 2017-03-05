c0747bd2667aa8585be9e53b58842426647ae7db
txw <goodboytxw@gmail.com>
2017-03-05 19:54:36 +0800

    register

--- resources/views/myapp/register.blade.php
+++ resources/views/myapp/register.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>H+ 后台主题UI框架 - 注册</title>
+    <title>注册</title>
     <link rel="shortcut icon" href="http://www.zi-han.net/theme/hplus/favicon.ico">
     <link href="{{ URL::asset('/bootstrap/css/bootstrap.css') }}" rel="stylesheet">
     <link href="{{ URL::asset('/fontawesome/css/font-awesome.min.css') }}" rel="stylesheet">
@@ -17,27 +17,38 @@
 
     <style>
         #BAIDU_DSPUI_FLOWBAR,.adsbygoogle,.ad,div[class^="ad-widsget"],div[id^="div-gpt-ad-"],a[href*="@"][href*=".exe"],a[href*="/?/"][href*=".exe"],.adpushwin{display:none !important;}
+
+        .form-group img {
+            width: 40%;
+            height: 32px;
+            float: right;
+            margin-top: 1px;
+            margin-bottom: 15px;
+        }
     </style>
 </head>
 
 <body class="gray-bg">
-
+<div style="margin-bottom: -50px">
+    <h1 class="logo-name"><center>T+M</center></h1>
+</div>
 <div class="middle-box text-center loginscreen   animated fadeInDown">
     <div>
-        <div>
-            <h1 class="logo-name">H+</h1>
-        </div>
-        <h3>欢迎注册 H+</h3>
-        <p>创建一个H+新账户</p>
+        <h3>欢迎注册 TalkingMore</h3>
+        <p>创建一个新账户</p>
         <form class="m-t" role="form" action="http://www.zi-han.net/theme/hplus/login.html">
             <div class="form-group">
-                <input type="text" class="form-control" placeholder="请输入用户名" required="">
+                <input type="text" class="form-control" placeholder="请输入邮箱" required="">
             </div>
             <div class="form-group">
                 <input type="password" class="form-control" placeholder="请输入密码" required="">
             </div>
             <div class="form-group">
                 <input type="password" class="form-control" placeholder="请再次输入密码" required="">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="请输入验证码" style="width: 60%;display: inline;">
+                <img src="{{ URL::asset('/image/user') }}">
             </div>
             <button type="submit" class="btn btn-primary block full-width m-b">注 册</button>
 
